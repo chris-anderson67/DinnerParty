db6c65b5714b11546aeffabd2c2000752396430a
chris-anderson67 <chrisja67@gmail.com>
2017-11-01 22:45:36 -0400

    updated gitignore

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from config import firebase_path
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +7,5 @@ def index():
     return "Hello World!"
 
 if __name__ == '__main__':
+    print firebase_path
     app.run()
