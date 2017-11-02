65cdfd6d7dbe622091b35d6b5f1ba3f2c44c6af7
chris-anderson67 <chrisja67@gmail.com>
2017-11-02 01:34:37 -0400

    inserting using unique keys, redirecting to success page

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from firebase import firebase
 from config import firebase_path
 from flask_wtf import FlaskForm as Form
+from flask_wtf.csrf import CSRFProtect
 from wtforms.fields import StringField, DateField
 from wtforms_components import TimeField
 from wtforms.validators import DataRequired
-from flask_wtf.csrf import CSRFProtect
-import datetime
-import json
-import random
+from datetime import date, time
+from uuid import uuid4
 
 app = Flask(__name__)
 firebase = firebase.FirebaseApplication(firebase_path, None)
-app.secret_key = "secret"
+app.secret_key = "secret" #secureAF
 
 class NewEvent(Form):
     owner_name = StringField('Your Name', validators=[DataRequired()])
@@ -26,7 +25,7 @@ class NewEvent(Form):
         data = self.data
         for field in data:
             value = data[field]
-            if type(value) is datetime.time or type(value) is datetime.date:
+            if type(value) is time or type(value) is date:
                 print value.isoformat()
                 data[field] = value.isoformat()
         return data
@@ -36,17 +35,21 @@ class NewEvent(Form):
 def test():
     return firebase.get('/test', None)
 
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
     form = NewEvent()
     if form.validate_on_submit():
-        curr_entries = firebase.get('/events')
-        new_key = random.randint(1, 100000000)
-        while (new_key not in curr_entries.keys):
-            new_key = random.randint(1, 100000000)
-
+        new_key = str(uuid4())
         firebase.put('/events', new_key, form.serialize_entry())
+        return redirect(url_for('view_event', event_id=new_key))
     return render_template('index.html', form=form)
+
+
+@app.route("/<event_id>", methods=['GET'])
+def view_event(event_id):
+    return str(firebase.get('/events/' + str(event_id), None))
+
 
 if __name__ == '__main__':
     app.run()
